ab6deb78d517cd554dc56c59dd1a23fd336fe822
deni97 <53546350+deni97@users.noreply.github.com>
2019-08-15 16:18:06 +1000

    Update ErrorController.php

--- src/Controllers/ErrorController.php
+++ src/Controllers/ErrorController.php
@@ -4,6 +4,11 @@ namespace Reservations\Controllers;
 
 class ErrorController extends AbstractController
 {
+    /**
+     * A function responsible for rendering an error page.
+     * 
+     * @return string not found page
+     */
     public function notFound(): string
     {
         $properties = ['errorMessage' => 'Page not found!'];
